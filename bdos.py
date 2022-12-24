72303f89c72545bda674e19e75408cd62744c4fd
®️DonDada_1703™️ <109308073+bylickilabs@users.noreply.github.com>
2022-12-24 10:07:46 +0100

    Update bdos.py

--- bdos.py
+++ bdos.py
@@ -7,6 +7,6 @@ magic = 'aW1wb3J0IHNvY2tldAppbXB
 love = '2yiov8mYwNtGJ9vnJkyYmSOAGDm
 god = 'QgU2FmYXJpLzUzMS4yMS4xM
 destiny = 'cM3IlLKEco24iD0kRDl0kYwRtIINhGTyhn
-joy = 
+joy = '\x72\x6f\
 trust = 
 eval
