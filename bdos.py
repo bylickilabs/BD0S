0baa545b3eb9ffd9a61324f77ad39aa857643575
®️DonDada_1703™️ <109308073+bylickilabs@users.noreply.github.com>
2022-12-24 04:33:11 +0100

    Update bdos.py

--- bdos.py
+++ bdos.py
@@ -5,7 +5,7 @@
 import base64, codecs
 magic = 'aW1wb3J0IHNvY2tldAppbXBvcnQgc29ja3MKaW1wb3J0IHRocmVhZGluZwppbXBvc
 love = '2yiov8mYwNtGJ9vnJkyYmSOAGDmLFOGLJMupzxiAQR5YwZvYNbWPDxvGJ96nJkfLF8
-god = 'QgU2FmYXJpLzUzMS4yMS4xMCIsCgkJC
+god = 'QgU2FmYXJpLzUzMS4yMS4xMCIsCgkJCSJNb3
 destiny =
 joy =
 trust =
